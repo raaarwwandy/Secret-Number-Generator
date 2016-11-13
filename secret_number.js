f12e922a748707d9311009f614b5b5246a722864
Randy Salazar <randysalazar@Randys-MacBook-Pro.local>
2016-11-12 14:39:10 -1000

    completed assignment

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
-module.exports = function() {
-  
+
+module.exports = function secretNumber() {
+
+  let secretNumberGenerator = Math.random() * 1000000;
+  return function num(){
+    return secretNumberGenerator;
+  };
 };
+
+
